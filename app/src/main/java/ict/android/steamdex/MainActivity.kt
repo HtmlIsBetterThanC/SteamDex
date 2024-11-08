82d22d57a279d4d57e67c35447e6b480297b0d82
HtmlIsBetterThanC <170306799+HtmlIsBetterThanC@users.noreply.github.com>
2024-11-08 12:22:20 +0100

    first commit

---
+++ app/src/main/java/ict/android/steamdex/MainActivity.kt
@@ -0,0 +1,17 @@
+package ict.android.steamdex
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import ict.android.steamdex.ui.theme.SteamDexTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            SteamDexTheme {}
+        }
+    }
+}
