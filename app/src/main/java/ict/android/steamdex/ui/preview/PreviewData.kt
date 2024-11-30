404e732c450da3a5beb62790fc5a95beca47eb52
Vanehir <pietro.sotgiu@edu.itspiemonte.it>
2024-11-30 16:54:17 +0100

    Squashed commit of the following:
    commit a9070ddeeaec502efd96c9d505a891517c9849c6
    Author: Vanehir <pietro.sotgiu@edu.itspiemonte.it>
    Date:   Sat Nov 30 16:53:44 2024 +0100
    
        feat: added Profile and PreviewData

---
+++ app/src/main/java/ict/android/steamdex/ui/preview/PreviewData.kt
@@ -0,0 +1,38 @@
+package ict.android.steamdex.ui.preview
+
+import ict.android.steamdex.models.ui.UiProfile
+
+object PreviewData {
+    val profiles = listOf(
+        UiProfile(
+            name = "Davanker",
+            iconUrl = "https://",
+            level = 49,
+            totalGames = 993,
+            totalHours = 2927.0,
+            playedGames = 289,
+            countryCode = "IT",
+            age = "12.4 years"
+        ),
+        UiProfile(
+            name = "Davanker2",
+            iconUrl = "https://",
+            level = 49,
+            totalGames = 993,
+            totalHours = 2927.0,
+            playedGames = 289,
+            countryCode = "IT",
+            age = "12.4 years"
+        ),
+        UiProfile(
+            name = "Davanker3",
+            iconUrl = "https://",
+            level = 49,
+            totalGames = 993,
+            totalHours = 2927.0,
+            playedGames = 289,
+            countryCode = "IT",
+            age = "12.4 years"
+        )
+    )
+}
