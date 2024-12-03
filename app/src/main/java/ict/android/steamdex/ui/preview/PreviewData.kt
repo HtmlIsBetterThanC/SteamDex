c18061ea6ffc12e1d03b5fa646a927138248edc4
Vanehir <pietro.sotgiu@edu.itspiemonte.it>
2024-12-03 12:27:57 +0100

    Squashed commit of the following:
    commit b5cb43534b5013c7d9728985518bc8f8e45ed738
    Author: Vanehir <pietro.sotgiu@edu.itspiemonte.it>
    Date:   Tue Dec 3 12:27:30 2024 +0100
    
        Squashed commit of the following:
    
        commit 6b42e7ab5aab93ea490767ee518c8e1992e8406c
        Author: HtmlIsBetterThanC <170306799+HtmlIsBetterThanC@users.noreply.github.com>
        Date:   Tue Dec 3 12:25:53 2024 +0100
    
            Squashed commit of the following:
    
            commit 618b1a3ce0b295716f671b4edd746a32c012093b
            Author: HtmlIsBetterThanC <170306799+HtmlIsBetterThanC@users.noreply.github.com>
            Date:   Tue Dec 3 12:24:49 2024 +0100
    
                chore: renamed dynamicColor to materialYou
    
            commit 2867faa11e5f3fe025a68fb5e55b6fce6780e6d0
            Author: HtmlIsBetterThanC <170306799+HtmlIsBetterThanC@users.noreply.github.com>
            Date:   Tue Dec 3 11:48:39 2024 +0100
    
                fix: set default value of dynamicColor to false
    
        commit 3fbd70ff92e88ce9189f490fe72622a910b0e01e
        Author: HtmlIsBetterThanC <170306799+HtmlIsBetterThanC@users.noreply.github.com>
        Date:   Tue Dec 3 12:06:57 2024 +0100
    
            feat: added BottomNavbar
    
        commit 5144bf1c6597e767643a79d333eac4d82c3e9c97
        Author: HtmlIsBetterThanC <170306799+HtmlIsBetterThanC@users.noreply.github.com>
        Date:   Tue Dec 3 12:04:07 2024 +0100
    
            fix: fixed wrong merged file
    
        commit 30613e36e99c9b2e6edcf24c68d86dc685fd5614
        Merge: 52eba77 119d118
        Author: HtmlIsBetterThanC <170306799+HtmlIsBetterThanC@users.noreply.github.com>
        Date:   Tue Dec 3 11:51:43 2024 +0100
    
            Merge branch 'navigation' into components
    
        commit 119d11874d8d2164fdb77b3a5b9199f6e6941e97
        Author: HtmlIsBetterThanC <170306799+HtmlIsBetterThanC@users.noreply.github.com>
        Date:   Tue Dec 3 11:45:48 2024 +0100
    
            feat: added list, strings and icons needed for BottomNavbar
    
        commit a94030240768815cd5920d532398a565c1f2f7ae
        Author: HtmlIsBetterThanC <170306799+HtmlIsBetterThanC@users.noreply.github.com>
        Date:   Tue Dec 3 11:41:43 2024 +0100
    
            fix: renamed HomeRoute to ExploreRoute
    
        commit 52eba77187070ce93e62a78ff31e574bb4044f20
        Author: HtmlIsBetterThanC <170306799+HtmlIsBetterThanC@users.noreply.github.com>
        Date:   Tue Dec 3 11:18:10 2024 +0100
    
            feat: added BackArrowButton
    
        commit dd2f0cdd9f595d1bfcf6b116282ef54b9b97944f
        Author: HtmlIsBetterThanC <170306799+HtmlIsBetterThanC@users.noreply.github.com>
        Date:   Tue Dec 3 11:07:52 2024 +0100
    
            feat: merged navigation branch
    
        commit 14b8d010394d0af1255926b5a7e9a265d5e7083d
        Author: HtmlIsBetterThanC <170306799+HtmlIsBetterThanC@users.noreply.github.com>
        Date:   Mon Dec 2 12:39:16 2024 +0100
    
            fix: removed parentheses from @Composable
    
        commit ee888b674f715cdecc711e89fdbd79c9952883d3
        Author: HtmlIsBetterThanC <170306799+HtmlIsBetterThanC@users.noreply.github.com>
        Date:   Mon Dec 2 12:37:51 2024 +0100
    
            feat: added ScreenTopBar
    
        commit 8a2ecc00edeffbdc3243f66eb2ffa6fa7338826f
        Author: HtmlIsBetterThanC <170306799+HtmlIsBetterThanC@users.noreply.github.com>
        Date:   Mon Dec 2 12:27:01 2024 +0100
    
            feat: added first implementation of NavigationHost
    
        commit 475221178d837a816617c1575ba1127154b8f475
        Author: HtmlIsBetterThanC <170306799+HtmlIsBetterThanC@users.noreply.github.com>
        Date:   Mon Dec 2 12:17:47 2024 +0100
    
            feat: added needed dependencies for navigation
    
    commit 735a516b338d26aa07df6006c5fbef7deaa456fd
    Author: Vanehir <pietro.sotgiu@edu.itspiemonte.it>
    Date:   Tue Dec 3 12:23:31 2024 +0100
    
        chore: added more preview profiles
    
    commit a9070ddeeaec502efd96c9d505a891517c9849c6
    Author: Vanehir <pietro.sotgiu@edu.itspiemonte.it>
    Date:   Sat Nov 30 16:53:44 2024 +0100
    
        feat: added Profile and PreviewData

---
+++ app/src/main/java/ict/android/steamdex/ui/preview/PreviewData.kt
@@ -0,0 +1,58 @@
+package ict.android.steamdex.ui.preview
+
+import ict.android.steamdex.models.ui.UiProfile
+
+object PreviewData {
+    val profiles = listOf(
+        UiProfile(
+            name = "Davanker",
+            iconUrl = "https://",
+            level = 49,
+            totalGames = 993,
+            totalHours = 2927.0,
+            playedGames = 289,
+            countryCode = "IT",
+            age = "12.4 years"
+        ),
+        UiProfile(
+            name = "Asuka_Unit02",
+            iconUrl = "https://",
+            level = 78,
+            totalGames = 1567,
+            totalHours = 4876.2,
+            playedGames = 423,
+            countryCode = "DE",
+            age = "9.8 years"
+        ),
+        UiProfile(
+            name = "NobodyKnownThatIAmSuS",
+            iconUrl = "https://",
+            level = 33,
+            totalGames = 567,
+            totalHours = 1845.3,
+            playedGames = 156,
+            countryCode = "CA",
+            age = "11.7 years"
+        ),
+        UiProfile(
+            name = "x%x_st1ll_w4t3t_x%x",
+            iconUrl = "https://",
+            level = 2001,
+            totalGames = 2345,
+            totalHours = 7896.7,
+            playedGames = 589,
+            countryCode = "US",
+            age = "12.9 years"
+        ),
+        UiProfile(
+            name = "Shinji_Unit01",
+            iconUrl = "https://",
+            level = 66,
+            totalGames = 1314,
+            totalHours = 3945.7,
+            playedGames = 389,
+            countryCode = "JP",
+            age = "14.6 years"
+        )
+    )
+}
