634621f0f4b1fe847ca12f4833fce45e968f4fa1
HtmlIsBetterThanC <170306799+HtmlIsBetterThanC@users.noreply.github.com>
2024-12-05 10:26:45 +0100

    feat: added qualifiers and modules for Coroutines

---
+++ app/src/main/java/ict/android/steamdex/di/Modules.kt
@@ -0,0 +1,37 @@
+package ict.android.steamdex.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DispatcherModule {
+    @DefaultDispatcher
+    @Provides
+    fun providesDefaultDispatcher(): CoroutineDispatcher = Dispatchers.Default
+
+    @IoDispatcher
+    @Provides
+    fun providesIoDispatcher(): CoroutineDispatcher = Dispatchers.IO
+
+    @MainDispatcher
+    @Provides
+    fun providesMainDispatcher(): CoroutineDispatcher = Dispatchers.Main
+}
+
+@InstallIn(SingletonComponent::class)
+@Module
+object CoroutinesScopesModule {
+    @Singleton
+    @Provides
+    fun providesCoroutineScope(): CoroutineScope {
+        return CoroutineScope(SupervisorJob() + Dispatchers.Default)
+    }
+}
