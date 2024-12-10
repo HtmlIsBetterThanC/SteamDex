710b96690c166ab7543ffa14b92f373328530fa2
HtmlIsBetterThanC <170306799+HtmlIsBetterThanC@users.noreply.github.com>
2024-12-10 17:02:40 +0100

    chore: added round corners and icon description to app icon

--- app/src/main/java/ict/android/steamdex/ui/screens/login/components/LogoAppIcon.kt
+++ app/src/main/java/ict/android/steamdex/ui/screens/login/components/LogoAppIcon.kt
@@ -1,7 +1,11 @@
 package ict.android.steamdex.ui.screens.login.components
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import coil3.compose.AsyncImage
 import ict.android.steamdex.R
 import ict.android.steamdex.ui.preview.PreviewSteam
@@ -13,8 +17,8 @@ fun LogoAppIcon(
 ) {
     AsyncImage(
         model = R.mipmap.ic_launcher_round,
-        contentDescription = null,
-        modifier = modifier
+        contentDescription = stringResource(R.string.app_icon_description),
+        modifier = modifier.clip(RoundedCornerShape(50.dp))
     )
 }
 
