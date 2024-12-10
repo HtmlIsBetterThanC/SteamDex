7d77b6f5473b68e2c860683bb3dea2c94066783e
HtmlIsBetterThanC <170306799+HtmlIsBetterThanC@users.noreply.github.com>
2024-12-10 17:46:47 +0100

    feat: added colors, text style and padding to LoginButton

--- app/src/main/java/ict/android/steamdex/ui/screens/login/components/LoginButton.kt
+++ app/src/main/java/ict/android/steamdex/ui/screens/login/components/LoginButton.kt
@@ -2,12 +2,16 @@ package ict.android.steamdex.ui.screens.login.components
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -21,20 +25,31 @@ fun LoginButton(
     onClick: () -> Unit,
     modifier: Modifier = Modifier
 ) {
+    val textAndIconColor = MaterialTheme.colorScheme.onTertiaryContainer
     PrimaryButton(
         onClick = onClick,
-        modifier = modifier
+        modifier = modifier.clip(RoundedCornerShape(100))
     ) {
         Row(
+            modifier = Modifier.padding(10.dp),
             horizontalArrangement = Arrangement.spacedBy(4.dp),
             verticalAlignment = Alignment.CenterVertically
         ) {
-            Text(stringResource(R.string.loginScreen_login_button_text))
+            Text(
+                stringResource(R.string.loginScreen_login_button_text),
+                color = textAndIconColor,
+                style = MaterialTheme.typography.labelLarge
+            )
             Icon(
                 painter = painterResource(R.drawable.steam_icon),
-                contentDescription = null
+                contentDescription = null,
+                tint = textAndIconColor
             )
-            Text(stringResource(R.string.steam))
+            Text(
+                stringResource(R.string.steam),
+                color = textAndIconColor,
+                style = MaterialTheme.typography.labelLarge
+            )
         }
     }
 }
@@ -44,7 +59,10 @@ fun LoginButton(
 private fun LoginButtonPreview() {
     SteamDexTheme {
         Surface {
-            LoginButton({})
+            LoginButton(
+                onClick = {},
+                modifier = Modifier.padding(8.dp)
+            )
         }
     }
 }
