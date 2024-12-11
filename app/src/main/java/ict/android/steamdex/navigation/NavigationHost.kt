18a727de14a6f1afc05ca8a8ffca951c43409f5f
HtmlIsBetterThanC <170306799+HtmlIsBetterThanC@users.noreply.github.com>
2024-12-11 11:14:50 +0100

    feat: added HomeScreen to navigation

--- app/src/main/java/ict/android/steamdex/navigation/NavigationHost.kt
+++ app/src/main/java/ict/android/steamdex/navigation/NavigationHost.kt
@@ -5,6 +5,11 @@ import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import ict.android.steamdex.navigation.graphs.aboutGraph
+import ict.android.steamdex.ui.components.BottomNavbar
+import ict.android.steamdex.ui.preview.PreviewData.games
+import ict.android.steamdex.ui.preview.PreviewData.profiles
+import ict.android.steamdex.ui.screens.home.HomeScreen
+import ict.android.steamdex.ui.screens.home.HomeUiState
 import ict.android.steamdex.ui.screens.login.LoginScreen
 
 @Composable
@@ -20,13 +25,31 @@ fun NavigationHost(
         startDestination = startDestination
     ) {
         fadeComposable<LoginRoute> {
-            val onLogin = {}
+            val onLogin = {
+                navController.navigate(ExploreRoute)
+            }
             LoginScreen(
                 onLogin = onLogin,
                 useGradientBackground = useGradientBackground
             )
         }
-        fadeComposable<ExploreRoute> { }
+        fadeComposable<ExploreRoute> {
+            val onProfileClick = {}
+            HomeScreen(
+                uiState = HomeUiState(
+                    profile = profiles[0],
+                    mostPlayedGames = games,
+                    trendingGames = games,
+                    onSaleGames = games.subList(0, 3),
+                    popularGames = games.subList(3, games.size - 1)
+                ),
+                useGradientBackground = useGradientBackground,
+                onProfileClick = onProfileClick,
+                onCategoryClick = {},
+                onSearchClick = {},
+                bottomBar = { BottomNavbar(navController) }
+            )
+        }
         fadeComposable<LibraryRoute> { }
         fadeComposable<GameRoute> { }
         fadeComposable<PublisherRoute> { }
