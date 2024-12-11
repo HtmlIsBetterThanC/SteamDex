85f8c410a6eeaa068e0c9484fb4931ede4571e55
HtmlIsBetterThanC <170306799+HtmlIsBetterThanC@users.noreply.github.com>
2024-12-12 00:22:57 +0100

    feat: implemented SteamPreferences

---
+++ app/src/main/java/ict/android/steamdex/ext/DatastoreExt.kt
@@ -0,0 +1,21 @@
+package ict.android.steamdex.ext
+
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.map
+
+fun <T> DataStore<Preferences>.observeValue(key: Preferences.Key<T>) = data.map { it[key] }
+
+suspend fun <T> DataStore<Preferences>.getValue(
+    key: Preferences.Key<T>,
+    default: T? = null,
+) = observeValue(key).first() ?: default
+
+suspend fun <T> DataStore<Preferences>.setValue(
+    key: Preferences.Key<T>,
+    value: T
+) = edit {
+    it[key] = value
+}
