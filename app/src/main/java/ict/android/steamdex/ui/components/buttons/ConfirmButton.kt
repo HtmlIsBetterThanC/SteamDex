54585ec33ba24a4e7f8e0291f6abad601baec709
HtmlIsBetterThanC <170306799+HtmlIsBetterThanC@users.noreply.github.com>
2024-12-12 11:52:25 +0100

    feat: added color parameters for confirm and cancel text

--- app/src/main/java/ict/android/steamdex/ui/components/buttons/ConfirmButton.kt
+++ app/src/main/java/ict/android/steamdex/ui/components/buttons/ConfirmButton.kt
@@ -1,10 +1,12 @@
 package ict.android.steamdex.ui.components.buttons
 
+import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import ict.android.steamdex.R
 import ict.android.steamdex.ui.preview.PreviewSteam
@@ -13,10 +15,14 @@ import ict.android.steamdex.ui.theme.SteamDexTheme
 @Composable
 fun ConfirmButton(
     onClick: () -> Unit,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    color: Color = LocalContentColor.current
 ) {
     TextButton(onClick, modifier) {
-        Text(stringResource(R.string.confirm_button_title))
+        Text(
+            stringResource(R.string.confirm_button_title),
+            color = color
+        )
     }
 }
 
