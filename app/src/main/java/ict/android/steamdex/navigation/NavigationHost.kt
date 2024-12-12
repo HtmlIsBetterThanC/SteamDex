f72ec5628d8d32ad9d1ab13584e73414f1e253ff
HtmlIsBetterThanC <170306799+HtmlIsBetterThanC@users.noreply.github.com>
2024-12-12 16:39:44 +0100

    feat: added Profile to NavigationHost

--- app/src/main/java/ict/android/steamdex/navigation/NavigationHost.kt
+++ app/src/main/java/ict/android/steamdex/navigation/NavigationHost.kt
@@ -1,10 +1,17 @@
 package ict.android.steamdex.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import ict.android.steamdex.navigation.graphs.aboutGraph
+import ict.android.steamdex.ui.components.BottomNavbar
+import ict.android.steamdex.ui.screens.profile.ProfileScreen
+import ict.android.steamdex.viewmodels.ProfileViewModel
 
 @Composable
 fun NavigationHost(
@@ -22,7 +29,60 @@ fun NavigationHost(
         fadeComposable<LibraryRoute> { }
         fadeComposable<GameRoute> { }
         fadeComposable<PublisherRoute> { }
-        fadeComposable<ProfileRoute> { }
+        fadeComposable<ProfileRoute> {
+            val viewModel = hiltViewModel<ProfileViewModel>()
+            val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+
+            val onCalculatorClick = {
+                navController.navigate(CalculatorRoute) {
+                    launchSingleTop = true
+                    restoreState = true
+                }
+            }
+            val onLanguageChange = { lang: String ->
+                // TODO implement language change
+            }
+            val onThemeChange = { darkTheme: Boolean? ->
+                viewModel.updateDarkTheme(darkTheme)
+            }
+            val onPitchBlackChange = { pitchBlack: Boolean ->
+                viewModel.updatePitchBlack(pitchBlack)
+                viewModel.updateGradientBackground(false)
+            }
+            val onMaterialYouChange = { materialYou: Boolean ->
+                viewModel.updateMaterialYou(materialYou)
+                viewModel.updateGradientBackground(false)
+            }
+            val onGradientBackgroundChange = { gradientBackground: Boolean ->
+                viewModel.updateGradientBackground(gradientBackground)
+            }
+            val onDefaultStartingScreenChange = { startingScreen: Any ->
+                viewModel.updateDefaultStartingScreen(startingScreen)
+            }
+            val onResetSettingClick = {
+                viewModel.resetAllSettings()
+            }
+
+            LaunchedEffect(null) {
+                viewModel.start()
+            }
+
+            ProfileScreen(
+                uiState = uiState,
+                useGradientBackground = false,
+                onCalculatorClick = onCalculatorClick,
+                onLanguageChange = onLanguageChange,
+                onThemeChange = onThemeChange,
+                onPitchBlackChange = onPitchBlackChange,
+                onMaterialYouChange = onMaterialYouChange,
+                onGradientBackgroundChange = onGradientBackgroundChange,
+                onDefaultStartingScreenChange = onDefaultStartingScreenChange,
+                onResetSettingClick = onResetSettingClick,
+                bottomBar = {
+                    BottomNavbar(navController)
+                }
+            )
+        }
         fadeComposable<CalculatorRoute> { }
         aboutGraph(navController)
     }
