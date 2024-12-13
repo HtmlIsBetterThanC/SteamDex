ad9c4ea9328904d9b04483cee3aa659282b65b27
HtmlIsBetterThanC <170306799+HtmlIsBetterThanC@users.noreply.github.com>
2024-12-13 18:13:09 +0100

    fix: added circleShape to ProfileImage

--- app/src/main/java/ict/android/steamdex/ui/components/ScreenTopBar.kt
+++ app/src/main/java/ict/android/steamdex/ui/components/ScreenTopBar.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -21,6 +22,7 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import ict.android.steamdex.ui.preview.PreviewSteam
@@ -48,7 +50,12 @@ fun ScreenTopBar(
                     .clickable { onProfileClick() },
                 horizontalArrangement = Arrangement.spacedBy(16.dp)
             ) {
-                SteamAsyncImage(profileIconUrl, Modifier.size(40.dp))
+                SteamAsyncImage(
+                    profileIconUrl,
+                    Modifier
+                        .size(40.dp)
+                        .clip(CircleShape)
+                )
                 Column {
                     Text(
                         profileName,
