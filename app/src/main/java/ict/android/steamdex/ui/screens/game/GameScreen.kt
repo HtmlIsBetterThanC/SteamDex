9efd1494fa3c293f907518fd44ef937bd90f56a0
Vanehir <pietro.sotgiu@edu.itspiemonte.it>
2024-12-15 09:19:25 +0100

    wip: gameScreenPage

--- app/src/main/java/ict/android/steamdex/ui/screens/game/GameScreen.kt
+++ app/src/main/java/ict/android/steamdex/ui/screens/game/GameScreen.kt
@@ -3,6 +3,7 @@ package ict.android.steamdex.ui.screens.game
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
@@ -12,56 +13,62 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import ict.android.steamdex.ui.components.buttons.SecondaryButton
 import ict.android.steamdex.R
+import ict.android.steamdex.ui.components.ScreenTopBar
 import ict.android.steamdex.ui.preview.PreviewSteam
+import ict.android.steamdex.ui.screens.components.AssistiveChipsRow
+import ict.android.steamdex.ui.theme.SteamDexTheme
 
 @Composable
 fun GameScreen(
+    uiState: GameUiState,
     onClickWhislist: () -> Unit,
     onClickFollow: () -> Unit,
     onClickIgnore: () -> Unit,
     onClickWatch: () -> Unit,
+    onProfileClick: () -> Unit,
     modifier: Modifier = Modifier
 ) {
     Scaffold(
-        modifier = modifier
+        modifier = modifier,
+        topBar = {
+            ScreenTopBar(
+                useGradientBackground = true,
+                profileIconUrl = uiState.profile.iconUrl,
+                profileName = uiState.profile.name,
+                profileLevel = uiState.profile.level,
+                onProfileClick = onProfileClick,
+            )
+        }
     ) { innerPadding ->
         Column(
-            modifier = Modifier.padding(innerPadding),
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxWidth(),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Row(){
-
-                SecondaryButton(
-                    label = stringResource(R.string.wishlist_button),
-                    onClick = onClickWhislist,
-                    enabled = true,
-                )
-
-                SecondaryButton(
-                    label = stringResource(R.string.follow_button),
-                    onClick = onClickFollow,
-                    enabled = true,
-                )
-
-                SecondaryButton(
-                    label = stringResource(R.string.ignore_button),
-                    onClick = onClickIgnore,
-                    enabled = true,
-                )
-                SecondaryButton(
-                    label = stringResource(R.string.watch_button),
-                    onClick = onClickWatch,
-                    enabled = true,
-                )
-            }
+            AssistiveChipsRow(
+                onClickWhislist = onClickWhislist,
+                onClickFollow = onClickFollow,
+                onClickIgnore = onClickIgnore,
+                onClickWatch = onClickWatch
+            )
         }
     }
 }
 
 @PreviewSteam
 @Composable
-private fun GameScreenPreview
-)
-     {
-    
+private fun GameScreenPreview(
+    @PreviewParameter(GamePreviewParameterProvider::class) uiState: GameUiState
+) {
+    SteamDexTheme {
+        GameScreen(
+            uiState = uiState,
+            onClickWhislist = {},
+            onClickFollow = {},
+            onClickIgnore = {},
+            onClickWatch = {},
+            onProfileClick = {}
+        )
+    }
 }
