c9e5003e724bcb7acc2733faf41d4967c07dfafd
HtmlIsBetterThanC <170306799+HtmlIsBetterThanC@users.noreply.github.com>
2024-12-16 11:57:16 +0100

    fix: added @Immutable

--- app/src/main/java/ict/android/steamdex/ui/screens/about/ButtonConfig.kt
+++ app/src/main/java/ict/android/steamdex/ui/screens/about/ButtonConfig.kt
@@ -1,12 +1,13 @@
 package ict.android.steamdex.ui.screens.about
 
+import androidx.compose.runtime.Immutable
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 
+@Immutable
 data class ButtonConfig(
     val icon: Painter,
     val text: String,
     val onClick: () -> Unit,
     val buttonColor: Color? = null
-
 )
