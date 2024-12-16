7496d48cfa24ff43cfd7558f970888efd3c2c2c7
HtmlIsBetterThanC <170306799+HtmlIsBetterThanC@users.noreply.github.com>
2024-12-16 16:05:15 +0100

    feat: added AboutAppScreen and DependenciesScreen to navigation

---
+++ app/src/main/java/ict/android/steamdex/Utils.kt
@@ -0,0 +1,9 @@
+package ict.android.steamdex
+
+const val SourceLink = "https://github.com/HtmlIsBetterThanC/SteamDex"
+const val DonationLink = "https://github.com/HtmlIsBetterThanC/SteamDex"
+const val TranslationLink = "https://github.com/HtmlIsBetterThanC/SteamDex"
+const val ReleasesLink = "https://github.com/HtmlIsBetterThanC/SteamDex/releases"
+const val LicenseLink = "https://github.com/HtmlIsBetterThanC/SteamDex/blob/main/LICENSE"
+const val PrivacyPolicyLink = "https://github.com/HtmlIsBetterThanC/SteamDex/blob/main/PrivacyPolicy.md"
+const val AuthorsLink = "https://github.com/HtmlIsBetterThanC/SteamDex/blob/main/Authors.md"
