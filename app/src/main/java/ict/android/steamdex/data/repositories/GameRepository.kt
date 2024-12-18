5f6cb3a32f669972489751d3553747d5f95048ab
HtmlIsBetterThanC <170306799+HtmlIsBetterThanC@users.noreply.github.com>
2024-12-18 03:34:19 +0100

    wip: updated GameRepository

--- app/src/main/java/ict/android/steamdex/data/repositories/GameRepository.kt
+++ app/src/main/java/ict/android/steamdex/data/repositories/GameRepository.kt
@@ -1,5 +1,7 @@
 package ict.android.steamdex.data.repositories
 
+import ict.android.steamdex.models.network.NetworkGame
+
 interface GameRepository {
     suspend fun isMostPlayedGamesExpanded(): Boolean
     suspend fun isTrendingGamesExpanded(): Boolean
@@ -10,4 +12,12 @@ interface GameRepository {
     suspend fun setTrendingGamesExpanded(expanded: Boolean)
     suspend fun setOnSaleGamesExpanded(expanded: Boolean)
     suspend fun setPopularGamesExpanded(expanded: Boolean)
+
+    suspend fun getMostPlayedGames(): List<NetworkGame>
+    suspend fun getTrendingGames(): List<NetworkGame>
+    suspend fun getOnSaleGames(): List<NetworkGame>
+    suspend fun getPopularGames(): List<NetworkGame>
+
+    suspend fun getOwnedGames(): List<NetworkGame>
+    suspend fun getGame(id: Long): NetworkGame
 }
--- app/src/main/java/ict/android/steamdex/data/repositories/GameRepositoryImpl.kt
+++ app/src/main/java/ict/android/steamdex/data/repositories/GameRepositoryImpl.kt
@@ -1,9 +1,20 @@
 package ict.android.steamdex.data.repositories
 
 import ict.android.steamdex.data.preferences.SteamPreferences
+import ict.android.steamdex.di.IoDispatcher
+import ict.android.steamdex.models.mappers.toNetworkModel
+import ict.android.steamdex.models.network.NetworkGame
+import ict.android.steamdex.ui.preview.PreviewData.games
+import io.ktor.client.HttpClient
 import jakarta.inject.Inject
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.withContext
 
-class GameRepositoryImpl @Inject constructor(private val preferences: SteamPreferences) : GameRepository {
+class GameRepositoryImpl @Inject constructor(
+    private val preferences: SteamPreferences,
+    @IoDispatcher private val ioDispatcher: CoroutineDispatcher
+) : GameRepository {
     override suspend fun isMostPlayedGamesExpanded(): Boolean {
         return preferences.getMostPlayedGamesExpanded()
     }
@@ -34,5 +45,38 @@ class GameRepositoryImpl @Inject constructor(private val preferences: SteamPrefe
 
     override suspend fun setPopularGamesExpanded(expanded: Boolean) {
         preferences.setPopularGamesExpanded(expanded)
+    }
+
+    override suspend fun getMostPlayedGames(): List<NetworkGame> {
+        return withContext(ioDispatcher) {
+            delay(5000L)
+            return@withContext games.map { it.toNetworkModel() }
+        }
+    }
+
+    override suspend fun getTrendingGames(): List<NetworkGame> {
+        return withContext(ioDispatcher) {
+            return@withContext games.subList(3, games.size).map { it.toNetworkModel() }
+        }
+    }
+
+    override suspend fun getOnSaleGames(): List<NetworkGame> {
+        return withContext(ioDispatcher) {
+            return@withContext games.subList(0, 4).map { it.toNetworkModel() }
+        }
+    }
+
+    override suspend fun getPopularGames(): List<NetworkGame> {
+        return withContext(ioDispatcher) {
+            return@withContext games.map { it.toNetworkModel() }
+        }
+    }
+
+    override suspend fun getOwnedGames(): List<NetworkGame> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getGame(id: Long): NetworkGame {
+        TODO("Not yet implemented")
     }
 }
