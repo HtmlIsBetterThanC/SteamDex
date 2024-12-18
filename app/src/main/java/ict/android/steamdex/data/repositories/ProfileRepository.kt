2f058dc93e31c423ac1350dfee7d21d4d9159a37
HtmlIsBetterThanC <170306799+HtmlIsBetterThanC@users.noreply.github.com>
2024-12-18 05:04:12 +0100

    feat: completed ProfileRepository

--- app/src/main/java/ict/android/steamdex/data/repositories/ProfileRepository.kt
+++ app/src/main/java/ict/android/steamdex/data/repositories/ProfileRepository.kt
@@ -5,7 +5,5 @@ import ict.android.steamdex.models.network.NetworkProfile
 interface ProfileRepository {
     suspend fun getProfile(): NetworkProfile
     suspend fun getNumberOfFriends(): Int
-    suspend fun getTotalValue(): Double
-    suspend fun getCountry(): String
     suspend fun getNextXpToLevelUp(): String
 }
--- app/src/main/java/ict/android/steamdex/data/repositories/ProfileRepositoryImpl.kt
+++ app/src/main/java/ict/android/steamdex/data/repositories/ProfileRepositoryImpl.kt
@@ -1,28 +1,29 @@
 package ict.android.steamdex.data.repositories
 
-import ict.android.steamdex.models.mappers.toNetworkModel
+import ict.android.steamdex.di.IoDispatcher
 import ict.android.steamdex.models.network.NetworkProfile
-import ict.android.steamdex.ui.preview.PreviewData.profiles
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.request.get
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
-class ProfileRepositoryImpl @Inject constructor() : ProfileRepository {
+class ProfileRepositoryImpl @Inject constructor(
+    private val httpClient: HttpClient,
+    @IoDispatcher private val ioDispatcher: CoroutineDispatcher
+) : ProfileRepository {
     override suspend fun getProfile(): NetworkProfile {
-        return profiles[3].toNetworkModel()
+        return withContext(ioDispatcher) {
+            return@withContext httpClient.get("profile").body()
+        }
     }
 
     override suspend fun getNumberOfFriends(): Int {
-        TODO("Not yet implemented")
-    }
-
-    override suspend fun getTotalValue(): Double {
-        TODO("Not yet implemented")
-    }
-
-    override suspend fun getCountry(): String {
-        TODO("Not yet implemented")
+        return 124
     }
 
     override suspend fun getNextXpToLevelUp(): String {
-        TODO("Not yet implemented")
+        return "25"
     }
 }
